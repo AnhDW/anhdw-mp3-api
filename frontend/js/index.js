5f1e2c259a8950ce3eb1363c1c6341a61421fd8f
AnhDW <vungocanh723@gmail.com>
2022-03-31 22:39:44 +0700

    cc

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -50,7 +50,7 @@ const pathName = () => {
         document.querySelector('#content_404').classList.add('active')
     }
     aElements.forEach((a, index) => {
-        if (a.href === (('http://localhost:' + ('3000' || process.env.PORT)) + path)) {
+        if (a.href === (('http://localhost:3000' || 'https://anhdw-mp3-api.herokuapp.com') + path)) {
             a.classList.add('active')
             contentItem[index].classList.add('active')
             document.querySelector('#content_404').classList.remove('active')
@@ -66,7 +66,7 @@ const pathName = () => {
 
 const pathSearch = () => {
     var path = window.location.pathname
-    var param = getUrlParameter('q', ('http://localhost:' + ('3000' || process.env.PORT)) + location.pathname + location.search)
+    var param = getUrlParameter('q', ('http://localhost:3000' || 'https://anhdw-mp3-api.herokuapp.com') + location.pathname + location.search)
     if (path === '/search') {
         keyword.value = param
         Search.start()
