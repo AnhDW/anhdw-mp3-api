1b669c64c7df96b6e9da1176c01a502f31152945
AnhDW <vungocanh723@gmail.com>
2022-04-19 21:05:04 +0700

    update

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const port = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, './frontend')))
 
-app.use('/api', apiRouters)
 app.use('/auth', authRouters)
 
 app.get('/*', (req, res) => {
