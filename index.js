9b36a7dab2a12bd464f4d363675dabc894113775
AnhDW <vungocanh723@gmail.com>
2022-04-21 13:33:22 +0700

    fix login

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const apiRouters = require('./routers/api')
-    //const authRouters = require('./routers/auth')
+const authRouters = require('./routers/auth')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000
 app.use(express.static(path.join(__dirname, './frontend')))
 
 app.use('/api', apiRouters)
-    //app.use('/auth', authRouters)
+app.use('/auth', authRouters)
 
 app.get('/*', (req, res) => {
     res.sendFile(__dirname + '/frontend/index.html')
